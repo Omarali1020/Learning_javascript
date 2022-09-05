f75e301bf1496a3be1c165c38b223c434816160f
Omarali1020 <93990428+Omarali1020@users.noreply.github.com>
2022-09-05 21:06:30 +0500

    lots

--- lecture_2/script.js
+++ lecture_2/script.js
@@ -1,22 +1,36 @@
 
-let Hello_World_button = document.getElementById('hello_world');
-Hello_World_button.addEventListener('click' , message)
+    console.log('This is the table of 2');
+    
+for( var i=2; i < 22; i = i+2 ){
+    console.log(i);
+    }
 
-function message (){
-    alert('Hello world');
+    console.log('This is the table of 3');
+
+for( var i=3; i < 33; i = i+3 ){
+    console.log(i);
+    }
+
+    console.log('This is the table of 4');
+
+for( var i=4; i < 44; i = i+4 ){
+    console.log(i);
+    }
+
+    console.log('This is the table of 5');
+
+for( var i=5; i < 55; i = i+5 ){
+    console.log(i);
+    }
+
+things = prompt(' what do you want ');
+thing = 'laptop';
+if( things == thing ){
+    console.log('i have dell precision m6600');
+
 }
 
-
-let hello_name  = document.getElementById('hello_name');
-hello_name.addEventListener('click' , input)
-
-function input (){
-  let name = prompt('name?')
-  hello_name.textContent = 'Hello!  ' + name 
-} 
-
-
-
-
-
-
+    else( ){
+    console.log('i do not have it ');
+    }
+    return
